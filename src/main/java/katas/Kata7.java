1e02ce0a4ac58c3776d425e48ac18715fe08da91
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-25 22:13:48 -0500

    Hecho
    Kata 7 resuelta

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -3,12 +3,14 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.BoxArt;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -19,6 +21,14 @@ public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+        List<Map> moviesMap = movieLists.stream()
+                .map(movie -> movie.getVideos())
+                .flatMap(list -> list.stream())
+                .map(movie -> ImmutableMap.of("id", movie.getId(), "title", movie.getTitle(),
+                        "boxart", movie.getBoxarts().stream().reduce((box1, box2) -> box1.getWidth() < box2.getWidth() ? box1 : box2).map(BoxArt::getUrl).get()))
+                .collect(Collectors.toList());
+
+        return moviesMap;
+        //ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
     }
 }
