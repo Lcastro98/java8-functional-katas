b9a0c5f7eac94bbbcd2e0d9f69f5d160fbedc68b
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-25 21:52:02 -0500

    Hecho
    Kata 6 resuelta

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,13 @@
 package katas;
 
+import com.google.common.collect.ImmutableMap;
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -14,6 +18,12 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        String url = movies.stream()
+                .map(movie -> movie.getBoxarts())
+                .flatMap(list -> list.stream())
+                .reduce((box1, box2) -> box1.getWidth() > box2.getWidth() ? box1 : box2)
+                .map(BoxArt::getUrl).get();
+
+        return url;
     }
 }
